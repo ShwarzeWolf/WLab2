7ee7936af57af03181fb8c7ac466c60a18d2382d
Kristin <1358365@DESKTOP-4FH9G41>
2019-11-19 20:43:32 +0300

    errors handling inWeatherBlock added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
 import React, {Component} from 'react';
 import './App.css';
 
-
 class WeatherBlock extends Component {
-    constructor() {
-        super();
+   constructor(props) {
+        super(props);
         this.state = {
-            weatherData: null
+            weatherData: undefined,
+            errorOccurred: false,
+            errorMessage: undefined,
+            cityName: props.cityName,
+            latitude: props.latitude,
+            longitude: props.longitude
         };
     }
 
-    componentDidMount() {
-        const name = this.props.name;
-        const URL = "http://api.openweathermap.org/data/2.5/weather?q=" +
-            name +
-            "&appid=f51bcfb8b207b0ef58ce10da80b90477";
+   formRequest(){
+        const name = this.state.cityName;
+        const latitude = this.state.latitude;
+        const longitude = this.state.longitude;
+
+        if (!!name)
+            return "http://api.openweathermap.org/data/2.5/weather?q=" +
+                name +
+                "&appid=f51bcfb8b207b0ef58ce10da80b90477";
+
+        if (!!latitude && !!longitude)
+            return "http://api.openweathermap.org/data/2.5/weather?lon=" +
+                longitude + "&lat=" + latitude +
+                "&appid=f51bcfb8b207b0ef58ce10da80b90477";
+
+        return null;
+    }
+
+   componentDidMount() {
+        const URL = this.formRequest();
 
         fetch(URL)
             .then(res => res.json())
             .then(json => {
-            this.setState({ weatherData: json });
+                if (json.cod === 200) {
+                    this.setState({errorOccurred: false});
+                    this.setState({errorMessage: undefined});
+                    this.setState({weatherData: json});
+                }
+                else {
+                    this.setState({errorMessage: json.message});
+                    this.setState({errorOccurred: true})
+                }
         });
     }
 
@@ -27,11 +54,18 @@ class WeatherBlock extends Component {
         const weatherData = this.state.weatherData;
 
         if (!weatherData)
-            return(<div>Подождите, данные загружаются</div>);
+            if (this.state.errorOccurred)
+                return(
+                    <div className="errorBlock">
+                        <div>Что-то пошло не так. Причина:</div>
+                        <div>{this.state.errorMessage}</div>
+                    </div>
+                );
+            else
+                return(<div>Подождите, данные загружаются</div>);
 
-        //return JSON.stringify(weatherData);
-        const weather = weatherData.weather[0];
-        const iconUrl = "http://openweathermap.org/img/w/" + weather.icon + ".png";
+
+        const iconUrl = "http://openweathermap.org/img/w/" + weatherData.weather[0].icon + ".png";
 
         return (
             <div class="weatherInfo">
@@ -49,10 +83,21 @@ class WeatherBlock extends Component {
 }
 
 class CurrentCity extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            cityName : "Surgut",
+            latitude : undefined,
+            longitude: undefined
+        }
+    }
+
     render(){
         return(
             <div className="currentCity">
-                <WeatherBlock name={"Surgut"} />
+                <h1>Погода здесь</h1>
+                <button>Обновить геолокацию</button>
+                <WeatherBlock cityName={this.state.cityName} latitude={this.state.latitude} longitude={this.state.longitude} />
             </div>
         )
     }
