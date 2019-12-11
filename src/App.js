bce5bd1ff48dc4286c526443d3af495fd5b669fe
Kristin <1358365@DESKTOP-4FH9G41>
2019-12-11 10:21:32 +0300

    last version I have

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
-import React, {Component} from 'react';
-import CurrentCity from './components/CurrentCity'
-import FavouriteCities from './components/FavouriteCities'
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-class App extends Component {
-    render() {
-        return (
-            <div className="App">
-                <CurrentCity/>
-                <FavouriteCities/>
-            </div>
-        );
-    }
+function App() {
+  return (
+    <div className="App">
+      <text>Hello, World!</text>
+    </div>
+  );
 }
 
 export default App;
